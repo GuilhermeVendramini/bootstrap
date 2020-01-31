119dcc2fa7562f7986f75ca58b07118590273d95
Guilherme <guilhermevendra@gmail.com>
2020-01-30 23:35:59 -0300

    Tests

--- test/app/core/core_widget_test.dart
+++ test/app/core/core_widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:bootstrap/app/core/core_module.dart';
 import 'package:bootstrap/app/core/core_widget.dart';
 import 'package:bootstrap/app/shared/utils/i18n/i18n_config.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -14,6 +15,7 @@ void main() {
     await tester.pumpWidget(
       buildTestableWidget(CoreWidget()),
     );
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
     await tester.pump(Duration(seconds: 3));
     final titleFinder = find.text(i18nDefault.appName.i18n);
     expect(titleFinder, findsNWidgets(2));
