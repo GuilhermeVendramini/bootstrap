9fd4157ee7616a8fef991f508091a2cbe48e5e03
Guilherme <guilhermevendra@gmail.com>
2020-02-06 21:23:44 -0300

    Not found page test

--- test/app/core/widgets/pages/not_found/core_not_found_page_test.dart
+++ test/app/core/widgets/pages/not_found/core_not_found_page_test.dart
@@ -1,11 +1,14 @@
 import 'package:bootstrap/app/core/widgets/pages/not_found/core_not_found_page.dart';
+import 'package:bootstrap/app/shared/utils/i18n/i18n_config.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
   testWidgets('Core NotFoundPage', (WidgetTester tester) async {
     await tester.pumpWidget(buildTestableWidget(CoreNotFoundPage()));
-    final titleFinder = find.text('NotFound');
+    final titleFinder = find.text(i18nDefault.notFoundPage.i18n);
+    final messageFinder = find.text(i18nDefault.notFoundMessage.i18n);
     expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
   });
 }
