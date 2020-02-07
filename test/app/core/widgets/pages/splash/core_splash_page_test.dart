5630877c9cd2d269ff99005c96e82d5a226a5696
Guilherme <guilhermevendra@gmail.com>
2020-02-06 21:19:58 -0300

    core_splash_page_test

--- test/app/core/widgets/pages/splash/core_splash_page_test.dart
+++ test/app/core/widgets/pages/splash/core_splash_page_test.dart
@@ -3,7 +3,6 @@ import 'package:bootstrap/app/core/core_module.dart';
 import 'package:bootstrap/app/core/widgets/pages/splash/core_splash_page.dart';
 import 'package:bootstrap/app/shared/utils/i18n/i18n_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -14,15 +13,31 @@ main() {
   setUpAll(() {
     Modular.init(CoreModule());
     coreController = CoreModule.to.get<CoreController>();
+    coreController.messageStatus = 'Default error message';
   });
 
-  testWidgets('Core Splash Page', (WidgetTester tester) async {
-    await tester.pumpWidget(MainTestWidget());
-    final titleFinder = find.text(i18nDefault.appName.i18n);
-    expect(titleFinder, findsNWidgets(1));
-    coreController.userLoadStatus = UserLoadStatus.LOADING;
-    final circularProgressFinder = find.byType(CircularProgressIndicator);
-    expect(circularProgressFinder, findsOneWidget);
+  group("[GROUP]: Core Splash Page", () {
+    testWidgets('Find Page Title', (WidgetTester tester) async {
+      await tester.pumpWidget(MainTestWidget());
+      final titleFinder = find.text(i18nDefault.appName.i18n);
+      expect(titleFinder, findsOneWidget);
+    });
+
+    testWidgets('Find CircularProgressIndicator', (WidgetTester tester) async {
+      await tester.pumpWidget(MainTestWidget());
+      coreController.userLoadStatus = UserLoadStatus.LOADING;
+      final circularProgressFinder = find.byType(CircularProgressIndicator);
+      expect(circularProgressFinder, findsOneWidget);
+    });
+
+    testWidgets('Find error message', (WidgetTester tester) async {
+      await tester.pumpWidget(MainTestWidget());
+
+      coreController.userLoadStatus = UserLoadStatus.ERROR;
+      await tester.pump();
+      final errorFinder = find.text(coreController.messageStatus);
+      expect(errorFinder, findsOneWidget);
+    });
   });
 }
 
